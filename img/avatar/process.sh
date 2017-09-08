8626ad58ba613d67835af259cc060cf29f9fdc8d
海豹 <007seadog@gmail.com>
2017-09-09 00:20:20 +0800

    Added avatar processing script

---
+++ img/avatar/process.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+[ -z "`which convert`" ] && echo "Missing convert" && exit 1
+[ -z "`which optipng`" ] && echo "Missing optipng" && exit 1
+[ -z "`which jpegoptim`" ] && echo "Missing jpegoptim" && exit 1
+for i in *.png; do convert $i -resize '100>' out.png; cp out.png $i; done
+for i in *.jpg; do convert $i -resize '100>' out.jpg; cp out.jpg $i; done
+parallel --bar 'optipng -o9 {}' ::: *.png
+parallel --bar 'jpegoptim -s {}' ::: *.jpg
