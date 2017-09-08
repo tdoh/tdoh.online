1ecd57edfcb1d346e1a6b85065f24e695547e60b
海豹 <007seadog@gmail.com>
2017-09-09 00:28:01 +0800

    Removing tmp file in process.sh

--- img/avatar/process.sh
+++ img/avatar/process.sh
@@ -6,5 +6,6 @@
 [ -z "`which jpegoptim`" ] && echo "Missing jpegoptim" && exit 1
 for i in *.png; do convert $i -resize '100>' out.png; cp out.png $i; done
 for i in *.jpg; do convert $i -resize '100>' out.jpg; cp out.jpg $i; done
+rm out.png out.jpg
 parallel --bar 'optipng -o9 {}' ::: *.png
 parallel --bar 'jpegoptim -s {}' ::: *.jpg
