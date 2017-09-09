d233a786227ba6468b64445e052810f0b1a011fe
海豹 <007seadog@gmail.com>
2017-09-09 21:08:45 +0800

    Added Teams Page

---
+++ js/team.js
@@ -0,0 +1,17 @@
+$.get('js/team.json', function(res){
+	console.log(res)
+	output = '';
+	for (var group in res) {
+		output += '<h3 class="position">' + group + '</h3>';
+		output += '<div class="row">';
+		for (var person in res[group]) {
+			output += '<div class="avator">';
+			output += '<img src="' + res[group][person].P + '">';
+			output += '<p class="name hm">' + res[group][person].N + '</p>';
+			output += '</div>';
+	    }
+		output += '</div>';
+		output += '<div class="spacing"></div>';
+	}
+	$('#teams').append(output);
+})
