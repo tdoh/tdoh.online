3c4d16d34eb50bd57aaaa09c2ee90aea926aedef
Aries Cs <aries0d0f@gmail.com>
2018-07-26 07:28:04 +0800

    New Page: add subpages

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/views/Home.vue'
+import views from '@/views'
 
 Vue.use(Router)
 
@@ -10,7 +10,58 @@ const router = new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      meta: {
+        label: '首頁'
+      },
+      component: views.Home
+    },
+    {
+      path: '/active',
+      name: 'Active',
+      meta: {
+        label: '大會活動'
+      },
+      component: views.Active
+    },
+    {
+      path: '/agenda',
+      name: 'Agenda',
+      meta: {
+        label: '議程資訊'
+      },
+      component: views.Agenda
+    },
+    {
+      path: '/traffic',
+      name: 'Traffic',
+      meta: {
+        label: '交通資訊'
+      },
+      component: views.Traffic
+    },
+    {
+      path: '/support',
+      name: 'Support',
+      meta: {
+        label: '協辦單位'
+      },
+      component: views.Support
+    },
+    {
+      path: '/team',
+      name: 'Team',
+      meta: {
+        label: '工作團隊'
+      },
+      component: views.Team
+    },
+    {
+      path: '/',
+      name: 'FloorMap',
+      meta: {
+        label: '場地平面圖'
+      },
+      component: views.FloorMap
     }
   ]
 })
---
+++ src/views/index.js
@@ -0,0 +1,17 @@
+import Home from './Home'
+import Active from './Active.vue'
+import Agenda from './Agenda.vue'
+import Traffic from './Traffic'
+import Support from './Support.vue'
+import Team from './Team.vue'
+import FloorMap from './FloorMap.vue'
+
+export default {
+  Home,
+  Active,
+  Agenda,
+  Traffic,
+  Support,
+  Team,
+  FloorMap
+}
