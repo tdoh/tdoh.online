d6cc091274e151920b914ced71c43ba8bc0c386a
Aries Cs <aries0d0f@gmail.com>
2018-07-26 07:10:47 +0800

    Initialize

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Footer from '@/components/Footer'
+export default {
+  components: {
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+@import './assets/scss/main.scss';
+</style>
---
+++ src/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+Vue.config.devtools = true
+
+const app = new Vue({
+  router,
+  ...App
+}).$mount('#app')
+
+export { app }
