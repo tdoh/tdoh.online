9509265cd57d0209cd6d3587764e150effc3dfb4
Aries Cs <aries0d0f@gmail.com>
2018-07-26 07:41:02 +0800

    Layout: basic navbar component

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,11 +1,21 @@
 <template>
   <div>
-
+    <ul id="nav">
+      <li v-for="(item, index) in menuItem" :key="index" :class="{ 'active': item.name === $route.name }">
+        <router-link :to="item.path">
+          <span>{{ item.meta.label || item.name }}</span>
+        </router-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  //
+  data () {
+    return {
+      menuItem: this.$router.options.routes
+    }
+  }
 }
 </script>
