58e33930f62104ee5462bc1387de1a8f4d91d87b
Aries Cs <aries0d0f@gmail.com>
2018-07-26 13:18:18 +0800

    Layout: menu
    - Add new color tickets
    - Add menu animate & icon
    - Almost stylesheets
    
    Signed-off-by: Aries Cs <aries0d0f@gmail.com>

--- src/components/layout/Navbar.vue
+++ src/components/layout/Navbar.vue
@@ -1,7 +1,8 @@
 <template>
-  <div>
-    <ul id="nav" :class="{ 'toggle': isToggle }">
-      <li v-for="(item, index) in menuItem" :key="index" :class="{ 'active': item.name === $route.name }">
+  <div id="nav">
+    <ul :class="{ 'toggle': isToggle }">
+      <img src="@/assets/images/icon/menu-active-gold.png" alt="" :class="`at-${activePage}`">
+      <li v-for="(item, index) in menuItem" :key="index">
         <router-link :to="item.path">
           <span>{{ item.meta.label || item.name }}</span>
         </router-link>
@@ -14,6 +15,7 @@
 export default {
   data () {
     return {
+      activePage: 0,
       menuItem: this.$router.options.routes
     }
   },
@@ -21,6 +23,19 @@ export default {
     isToggle: {
       default: false,
       type: Boolean
+    }
+  },
+  mounted () {
+    this.getActivePage()
+  },
+  methods: {
+    getActivePage () {
+      this.activePage = this.$router.options.routes.indexOf(this.$router.options.routes.filter((item, index) => (item.name === this.$route.name))[0])
+    }
+  },
+  watch: {
+    $route: function () {
+      this.getActivePage()
     }
   }
 }
