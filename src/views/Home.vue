c79afd74e449f35f5e715d64cd5539a51a35a0b3
Aries Cs <aries0d0f@gmail.com>
2018-07-26 21:21:14 +0800

    Content: add traffic link

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,7 +9,7 @@
             <h2>TDOH Conf</h2>
             <h1>2018</h1>
             <h3>駭客地下城</h3>
-            <p>地點：蓮潭國際會館</p>
+            <p>地點：<a href="https://ppt.cc/fgKQWx">高雄蓮潭國際會館</a></p>
           </div>
           <a href="" class="button" target="_blank">8／18 早鳥票開跑</a>
           <Counter v-if="new Date('2018-08-18').getTime() > new Date().getTime()" />
