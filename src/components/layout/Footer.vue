3df3cdc0712a935288d16e234baacd29cdccc906
Aries Cs <aries0d0f@gmail.com>
2018-07-26 17:53:29 +0800

    Layout: add RWD

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="footer">
-    <p>&copy; TDOHacker 2018｜主辦單位 TDOHacker｜承辦單位 臺灣校園資訊安全推廣暨駭客培育協會｜技術協力 LIT Team</p>
+    <p>&copy; TDOHacker 2018</p>
+    <p>主辦單位｜TDOHacker</p>
+    <p>承辦單位｜臺灣校園資訊安全推廣暨駭客培育協會</p>
+    <p>技術協力｜LIT Team</p>
   </div>
 </template>
 
