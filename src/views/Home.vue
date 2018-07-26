a25bd994dc16068773a315a4c6b81241014d17a3
Aries Cs <aries0d0f@gmail.com>
2018-07-26 15:17:06 +0800

    Layout: modify content

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,13 +4,14 @@
       <div class="container">
         <img id="logo" src="../assets/images/logo-big.png" alt="">
         <div class="content">
-          <p class="tag">9／28 ～ 9／29</p>
+          <p class="tag">9／29</p>
           <div class="title-box">
             <h2>TDOH Conf</h2>
             <h1>2018</h1>
             <h3>駭客地下城</h3>
+            <p>地點：蓮潭國際會館</p>
           </div>
-          <a href="" class="button" target="_blank">立即報名</a>
+          <a href="" class="button" target="_blank">8／18 早鳥票開跑</a>
         </div>
       </div>
     </div>
