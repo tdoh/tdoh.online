87cf3b60d73586127e967f44dcecab8eb3cc8b16
Aries Cs <aries0d0f@gmail.com>
2018-07-26 19:47:32 +0800

    Content: update footer link

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -3,7 +3,7 @@
     <p>&copy; TDOHacker 2018</p>
     <p>主辦單位｜TDOHacker</p>
     <p>承辦單位｜臺灣校園資訊安全推廣暨駭客培育協會</p>
-    <p>技術協力｜LIT Team</p>
+    <p>技術協力｜<a href="https://open.limeishu.org.tw">LIT Team</a></p>
   </div>
 </template>
 
