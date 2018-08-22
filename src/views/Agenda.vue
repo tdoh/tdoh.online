dfe0ca028c0b1905ec03a107f0eb666572ce0ec4
Aries Cs <aries0d0f@gmail.com>
2018-08-22 20:28:16 +0800

    Layout: add comming soon landing msg

--- src/views/Agenda.vue
+++ src/views/Agenda.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
-
+    <div class="spotlight">
+      <div class="spotlight-box">
+        <h1 class="title symbol"><span>Comming Soon</span></h1>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/views/FloorMap.vue
+++ src/views/FloorMap.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="spotlight">
       <div class="spotlight-box">
-        <h1 class="title symbol"><span>場地平面圖</span></h1>
+        <h1 class="title symbol"><span>Comming Soon</span></h1>
       </div>
     </div>
   </div>
