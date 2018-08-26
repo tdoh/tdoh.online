0565f0a3022d4c1b5cf041dc6d748494aec5af4c
Aries Cs <aries0d0f@gmail.com>
2018-08-26 15:20:50 +0800

    Layout: add team stylesheets and sample data

--- src/views/Team.vue
+++ src/views/Team.vue
@@ -1,8 +1,14 @@
 <template>
   <div>
     <div class="spotlight">
-      <div class="spotlight-box">
-        <h1 class="title symbol"><span>Comming Soon</span></h1>
+      <div class="spotlight-box member" v-for="team in teams" :key="team.type">
+        <h1 class="title symbol"><span>{{ team.type }}</span></h1>
+        <div class="spotlight-member">
+          <div class="member" v-for="(member, i) in team.members" :key="i">
+            <img :src="require(`@/assets/images/team/${member.image}`)" alt="">
+            <p>{{ member.name }}</p>
+          </div>
+        </div>
       </div>
     </div>
     <Footer />
@@ -12,6 +18,91 @@
 <script>
 import layout from '@/components/layout'
 export default {
+  data () {
+    return {
+      teams: [
+        {
+          type: '總招組',
+          members: [
+            {
+              name: 'HrJ',
+              image: 'image29.jpg'
+            },
+            {
+              name: 'Syu',
+              image: 'image29.jpg'
+            },
+            {
+              name: 'HrJ',
+              image: 'image29.jpg'
+            },
+            {
+              name: 'Syu',
+              image: 'image29.jpg'
+            }
+          ]
+        },
+        {
+          type: '行政組',
+          members: [
+            {
+              name: 'HrJ',
+              image: 'image29.jpg'
+            },
+            {
+              name: 'Syu',
+              image: 'image29.jpg'
+            }
+          ]
+        },
+        {
+          type: '場務組',
+          members: [
+            {
+              name: 'HrJ',
+              image: 'image29.jpg'
+            },
+            {
+              name: 'Syu',
+              image: 'image29.jpg'
+            },
+            {
+              name: 'HrJ',
+              image: 'image29.jpg'
+            },
+            {
+              name: 'Syu',
+              image: 'image29.jpg'
+            },
+            {
+              name: 'HrJ',
+              image: 'image29.jpg'
+            },
+            {
+              name: 'Syu',
+              image: 'image29.jpg'
+            },
+            {
+              name: 'HrJ',
+              image: 'image29.jpg'
+            },
+            {
+              name: 'Syu',
+              image: 'image29.jpg'
+            },
+            {
+              name: 'HrJ',
+              image: 'image29.jpg'
+            },
+            {
+              name: 'Syu',
+              image: 'image29.jpg'
+            }
+          ]
+        }
+      ]
+    }
+  },
   components: {
     ...layout
   }
