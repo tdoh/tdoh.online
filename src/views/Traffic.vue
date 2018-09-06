d496ec86a468c4e7866fbcd90c9879a7064c615e
Aries Cs <aries0d0f@gmail.com>
2018-09-06 15:02:59 +0800

    Layout: update traffic information

--- src/views/Traffic.vue
+++ src/views/Traffic.vue
@@ -6,19 +6,19 @@
         <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14725.429635784727!2d120.2908224!3d22.6777352!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x346e050a5586c935%3A0xbf3a2e2c964d999!2z6auY6ZuE6JOu5r2t5ZyL6Zqb5pyD6aSoIEdhcmRlbnZpbGxh!5e0!3m2!1szh-TW!2stw!4v1534230360981" frameborder="0" style="border:0" allowfullscreen></iframe>
       </div>
       <div class="spotlight-box">
-        <h1 class="title symbol">公車</h1>
+        <h1 class="title symbol"><span>公車</span></h1>
         <div class="content">
           <p>搭乘 E11A（鳳山高鐵城市快線）、E11B（鳳山高鐵城市快線）、紅35A（包含有繞崇實社區）、紅35B、紅51B（包含公車式小黃）、38A、38B、38C、38D、301A、301B 或 8021 至蓮潭會館。</p>
         </div>
       </div>
       <div class="spotlight-box">
-        <h1 class="title symbol">捷運</h1>
+        <h1 class="title symbol"><span>捷運</span></h1>
         <div class="content">
           <p>搭乘 紅線 於生態園區站下車，由 2號出口 出站，並轉乘 紅35A（包含有繞崇實社區）、紅35B 或 紅51B 至蓮潭會館。</p>
         </div>
       </div>
       <div class="spotlight-box">
-        <h1 class="title symbol">高鐵</h1>
+        <h1 class="title symbol"><span>高鐵</span></h1>
         <div class="content">
           <p>搭乘至 高鐵左營站 下車：</p>
           <ul>
@@ -28,7 +28,7 @@
         </div>
       </div>
       <div class="spotlight-box">
-        <h1 class="title symbol">臺鐵</h1>
+        <h1 class="title symbol"><span>臺鐵</span></h1>
         <div class="content">
           <p>搭乘至 臺鐵新左營站 下車：</p>
           <ul>
@@ -38,7 +38,7 @@
         </div>
       </div>
       <div class="spotlight-box">
-        <h1 class="title symbol">客運</h1>
+        <h1 class="title symbol"><span>客運</span></h1>
         <div class="content">
           <p>高雄車站週邊有多家客運業者提供客運服務，路線遍及各縣市</p>
           <ul>
@@ -52,7 +52,7 @@
         </div>
       </div>
       <div class="spotlight-box">
-        <h1 class="title symbol">開車</h1>
+        <h1 class="title symbol"><span>開車</span></h1>
         <div class="content">
           <ul>
             <li>
@@ -73,7 +73,7 @@
         </div>
       </div>
       <div class="spotlight-box">
-        <h1 class="title symbol">航空</h1>
+        <h1 class="title symbol"><span>航空</span></h1>
         <div class="content">
           <p>到達 高雄國際航空站（高雄國際機場）後，轉乘捷運從 高雄國際機場站 搭到 生態園區站 下車，由 2號出口 出站，再轉乘 紅35A（包含有繞崇實社區）、紅35B 或 紅51B 至蓮潭會館。</p>
         </div>
