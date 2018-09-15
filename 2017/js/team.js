d71a7074c8b179d8ce42157fbc0387fe01df026d
Aries Cs <aries0d0f@gmail.com>
2018-09-16 00:49:46 +0800

    Init

---
+++ 2017/js/team.js
@@ -0,0 +1,19 @@
+$.get('js/team.json', function(res){
+	console.log(res)
+	output = '';
+	for (var group in res) {
+		output += '<div class="team-row">';
+		output += '<h3>' + group + '</h3>';
+		output += '<div class="row">';
+		for (var person in res[group]) {
+			output += '<div class="avator">';
+			output += '<img src="' + (res[group][person].P || "img/avatar/image0.png") + '">';
+			output += '<p class="name hm">' + res[group][person].N + '</p>';
+			output += '</div>';
+	    }
+		output += '</div>';
+		output += '</div>';
+		output += '<div class="spacing"></div>';
+	}
+	$('#teams').append(output);
+})
